43b83faae5b996f95d82db722d26a387ba7b6c2a
Daniel <darkshoxx@yahoo.de>
2022-10-23 09:13:52 +0100

    first demo

--- Piano-To-Text/run_xdl.py
+++ Piano-To-Text/run_xdl.py
@@ -1,4 +1,10 @@
-import XDL
-from Chemputer import ChemputerPlatform
+from xdl import XDL
+# This obviously only works in the ChemPU Stack
+from chemputerxdl import ChemputerPlatform
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+xdl_file = os.path.join(HERE, "my_file.xdl")
 
 x = XDL(xdl_file, platform=ChemputerPlatform)
+print(x.human_readable())
